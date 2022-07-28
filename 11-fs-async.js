30e62fcabedf7ab68e78628c611a8c52e842e226
dawkey95 <dawidkeyser95@gmail.com>
2022-07-29 08:54:56 +1200

    Difference between sync and async using logs

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,5 +1,7 @@
 const {readFile, writeFile} = require('fs');
 
+console.log('Starting');
+
 readFile('./content/first.txt', 'utf8', (err, result) => {
     if (err) {
         console.log(err);
@@ -18,7 +20,9 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
                 console.log(err);
                 return
             }
-            console.log(result);
+            console.log('Done with this task');
         })
     })
 })
+
+console.log('Starting next task');
